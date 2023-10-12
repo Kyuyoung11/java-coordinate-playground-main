fcf324e839c7e2157debb0f0ccf99310f9c0967e
Kyuyoung11 <58154672+Kyuyoung11@users.noreply.github.com>
2023-10-13 00:56:09 +0900

    [fix] Utils -> Domain object로 구현

--- src/test/java/coordinate/PointCalculatorTest.java
+++ src/test/java/coordinate/PointCalculatorTest.java
@@ -1,19 +1,41 @@
 package coordinate;
 
 import coordinate.domains.Point;
-import coordinate.utils.PointCalculatorUtils;
+import coordinate.domains.PointCalculator;
+import coordinate.domains.Points;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PointCalculatorTest {
-    @Test
-    void 길이계산_테스트() {
+    private PointCalculator pointCalculator;
+
+    @BeforeEach
+    void setUp() {
+
         Point p1 = new Point(1,1);
         Point p2 = new Point(1,2);
 
-        double distance = Math.round(PointCalculatorUtils.calcDistance(p1, p2));
+
+        pointCalculator = new PointCalculator(new Points(Arrays.asList(p1,p2)));
+    }
+
+    @Test
+    void String생성자_테스트() {
+        pointCalculator = new PointCalculator("(10,10)-(14,15)");
+    }
+
+
+
+    @Test
+    void 길이계산_테스트() {
+
+        double distance = Math.round(pointCalculator.calcDistance());
         System.out.println(distance);
         assertEquals(Math.round(distance), 1);
     }
+
 }
