049272af81032e5102828e6937e336727b92c60b
Kyuyoung11 <58154672+Kyuyoung11@users.noreply.github.com>
2023-10-15 23:51:48 +0900

    [feat] string to Point

--- src/main/java/coordinate/utils/ParsingUtils.java
+++ src/main/java/coordinate/utils/ParsingUtils.java
@@ -3,13 +3,26 @@ package coordinate.utils;
 import coordinate.domains.Point;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ParsingUtils {
-    public static List<String> convertToPointList(String coordinate) {
+    public static List<Point> convertToPointList(String coordinate) {
+        //1. - 기준으로 split
+        //2.
         return null;
     }
 
-    public static Point convertToPoint() {
-        return null;
+    public static Point convertToPoint(String point) {
+        Pattern pattern = Pattern.compile("[(](.*?)[,](.*?)[)]");
+        Matcher matcher = pattern.matcher(point);
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("올바른 좌표 문자열이 아닙니다.");
+        }
+        return new Point(converToInt(matcher.group(1)), converToInt(matcher.group(2)));
+    }
+
+    public static int converToInt(String str) {
+        return Integer.parseInt(str);
     }
 }
