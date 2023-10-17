5ce6d95e2b53d06c44fd24387e38af5a90d1e8cc
Kyuyoung11 <kyuyoung.lee@bankwareglobal.com>
2023-10-17 12:56:11 +0900

    (fix) 메소드명 변경

--- src/main/java/coordinate/view/OutputView.java
+++ src/main/java/coordinate/view/OutputView.java
@@ -4,7 +4,7 @@ import coordinate.domains.PointCalculator;
 
 public class OutputView {
     public static void printDistance(PointCalculator pointCalculator) {
-        double distance = pointCalculator.calcDistance();
+        double distance = pointCalculator.calculateArea();
         System.out.printf("두 점 사이 거리는 %.6f%n", distance);
     }
 }
