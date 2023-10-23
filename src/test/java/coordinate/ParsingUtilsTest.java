a3210285bdf002c48bd4a947d3a60a21d0f8aa4e
Kyuyoung11 <58154672+Kyuyoung11@users.noreply.github.com>
2023-10-24 00:05:01 +0900

    [feat] 직사각형 추가 테스트

--- src/test/java/coordinate/ParsingUtilsTest.java
+++ src/test/java/coordinate/ParsingUtilsTest.java
@@ -28,4 +28,25 @@ public class ParsingUtilsTest {
         assertTrue(p2.isEqualsXY(14,15));
 
     }
+
+    @Test
+    void convertToPointList2() {
+        List<Point> pointList = ParsingUtils.convertToPointList("(10,10)-(22,10)-(22,18)-(10,18)");
+        for(Point p: pointList){
+            System.out.println(p.toString());
+        }
+
+        Point p1 = pointList.get(0);
+        assertTrue(p1.isEqualsXY(10,10));
+
+        Point p2 = pointList.get(1);
+        assertTrue(p2.isEqualsXY(22,10));
+
+        Point p3 = pointList.get(2);
+        assertTrue(p3.isEqualsXY(22,18));
+
+        Point p4 = pointList.get(3);
+        assertTrue(p4.isEqualsXY(10,18));
+
+    }
 }
