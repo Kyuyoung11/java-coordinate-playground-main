e379f860e07c5b0723164b4e0f8eae989e8142cf
Kyuyoung11 <58154672+Kyuyoung11@users.noreply.github.com>
2023-10-24 00:17:44 +0900

    [feat] 메인 작성

--- src/main/java/coordinate/Main.java
+++ src/main/java/coordinate/Main.java
@@ -7,12 +7,12 @@ import coordinate.view.OutputView;
 public class Main {
     public static void main(String[] args) {
         //1.입력 받기
-        String coordinateInput = InputView._getCoordinateInput();
+        String coordinateInput = InputView.getCoordinateInput();
 
         //2.좌표계산기 생성
         PointCalculator pointCalculator = new PointCalculator(coordinateInput);
 
         //3.거리 출력
-        OutputView.printDistance(pointCalculator);
+        OutputView.printArea(pointCalculator);
     }
 }
