658df95ec892ba68700e7996a3b9bbe6386b3f7d
Kyuyoung11 <58154672+Kyuyoung11@users.noreply.github.com>
2023-10-29 23:54:16 +0900

    [feat] if문 없애기

--- src/main/java/coordinate_feedback/FigureFactory.java
+++ src/main/java/coordinate_feedback/FigureFactory.java
@@ -2,22 +2,42 @@ package coordinate_feedback;
 
 
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
 
 public class FigureFactory {
+    private static Map<Integer, Supplier<Figure>> figures;
+
     static Figure getInstance(List<Point> points) {
-        if (points.size() == Line.LINE_POINT_SIZE) {
-            return new Line(points);
+        figures = new HashMap<>();
+        figures.put(2, (Supplier<Figure>) new Line(points));
+        figures.put(3, (Supplier<Figure>) new Triangle(points));
+        figures.put(4, (Supplier<Figure>) new Rectangle(points));
+
+        try {
+            return figures.get(points.size()).get();
+        } catch(NullPointerException e) {
+            throw new IllegalArgumentException("유효하지 않은 도형입니다.");
         }
 
-        if (points.size() == Triangle.TRIANGLE_POINT_SIZE) {
-            return new Triangle(points);
-        }
 
-        if (points.size() == Rectangle.RECTANGLE_POINT_SIZE) {
-            return new Rectangle(points);
-        }
-
-        throw new IllegalArgumentException("유효하지 않은 도형입니다.");
+//        if (points.size() == Line.LINE_POINT_SIZE) {
+//            return new Line(points);
+//        }
+//
+//        if (points.size() == Triangle.TRIANGLE_POINT_SIZE) {
+//            return new Triangle(points);
+//        }
+//
+//        if (points.size() == Rectangle.RECTANGLE_POINT_SIZE) {
+//            return new Rectangle(points);
+//        }
+//
+//        throw new IllegalArgumentException("유효하지 않은 도형입니다.");
     }
+
+
+
 }
